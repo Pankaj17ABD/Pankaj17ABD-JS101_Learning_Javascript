aacc4f9a23abcc45c62ab58adb70355444c0ee7a
Pankaj17ABD <rwtpnkj1718@gmail.com>
2023-01-12 20:34:01 +0530

    day 4 while loops

---
+++ day 4 while loops/Problem5.js
@@ -0,0 +1,21 @@
+//Fix the given flowchart.
+
+//Below is the flowchart of computing the following sum :
+// 0 + 5 + 10 + 15 + 20 ......
+
+//There is an error in the following flowchart - Identify it.
+//Submit Line number and Corrected text in your answer, 
+//For Example : Line 3 should be "Is n<3?”
+
+let j=0;
+let sum=0;
+let n=5*i;
+for(let i=1; i<=n; i++){
+  sum=sum+i
+  j=j+5
+  i=i+1
+  console.log(sum);
+}
+  
+  
+
---
+++ day 4 while loops/problem3.js
@@ -0,0 +1,7 @@
+let sum = 0;
+for (let a = 1; a <= 10; a++) {
+  if (a % 3 == 0)
+    sum = sum + a;
+
+}
+console.log(sum);
